aaee1ccf55dcd7ea422c0872ac55e62bbf228d75
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-08 16:38:38 +0200

    [UPD] added for to create 100 cells

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //Creo una funzione per mettere dentro la grid un quadrato
-function createSingleGrid(num) {
+function createSingleSquare(num) {
     //creo il quadrato come div
     const square = document.createElement('div');
     //aggiungo la classe square al div
     square.classList.add('square');
     //aggiungo il numero dentro il div square
-    square.innerText = num;
+    square.innerText = num + 1;
 }
 
 //Recupero il pulsante dall HTML
@@ -16,6 +16,10 @@ button.addEventListener('click' function () {
 
     //Recupero l'elemento che conterrà la griglia
     const grid = document.getElementById('grid');
-
+    //ciclo per creare 100 caselle 
+    for (let i = 0; i < 100; i++) {
+        //chiamo la funzione per creare le caselle
+        createSingleSquare(i);
+    }
 });
 
