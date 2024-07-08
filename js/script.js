3159e42ceac8dc6f0f6ddd256422837c29f2da45
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-08 16:35:24 +0200

    [UPD] Created function for create square inside the grid

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+//Creo una funzione per mettere dentro la grid un quadrato
+function createSingleGrid(num) {
+    //creo il quadrato come div
+    const square = document.createElement('div');
+    //aggiungo la classe square al div
+    square.classList.add('square');
+    //aggiungo il numero dentro il div square
+    square.innerText = num;
+}
+
 //Recupero il pulsante dall HTML
 const button = document.getElementsByName('play');
 
 //Aggiungo l'evento
 button.addEventListener('click' function () {
 
+    //Recupero l'elemento che conterrà la griglia
+    const grid = document.getElementById('grid');
+
 });
+
